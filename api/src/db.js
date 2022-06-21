ca72e818534befe8f1f29ffd320062c9737a6eba
John Arango <johnj42@gmail.com>
2022-06-21 10:37:05 -0500

    nodemon removed

--- api/src/db.js
+++ api/src/db.js
@@ -3,11 +3,17 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST} = process.env;
+  DATABASE_URL} = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthirized: false
+    }
+  }
 });
 const basename = path.basename(__filename);
 
